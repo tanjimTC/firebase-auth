06c2f5e0543620d8a40c2b3c982c325168e00257
tanjimTC <tanjimtc74@gmail.com>
2020-09-23 22:16:36 +0600

    cloud function added for admin role

---
+++ functions/index.js
@@ -0,0 +1,24 @@
+const functions = require("firebase-functions");
+
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+exports.addAdminRole = functions.https.onCall((data, context) => {
+  // get user and set custom claim
+  return admin
+    .auth()
+    .getUserByEmail(data.email)
+    .then((user) => {
+      return admin.auth().setCustomUserClaims(user.uid, {
+        admin: true,
+      });
+    })
+    .then(() => {
+      return {
+        message: `Success ${data.email} has been made an Admin`,
+      };
+    })
+    .catch((err) => {
+      return err;
+    });
+});
--- scripts/auth.js
+++ scripts/auth.js
@@ -109,3 +109,15 @@ firebase.auth().onAuthStateChanged((user) => {
     displayUSer();
   }
 });
+
+// add admin cloud function
+const adminForm = document.querySelector(".admin-actions");
+adminForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const adminEmail = document.querySelector("#admin-email").value;
+  const addAdminRole = functions.httpsCallable("addAdminRole");
+  addAdminRole({ email: adminEmail }).then((result) => {
+    console.log(result);
+  });
+});
